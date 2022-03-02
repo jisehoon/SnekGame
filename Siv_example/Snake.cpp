74ad9a9cf1d964c5343db269fda1dad5daa78aeb
jisehoon <jisehoon3@gmail.com>
2022-03-02 20:31:23 +0900

    SnekGame Complete

---
+++ Siv_example/Snake.cpp
@@ -0,0 +1,75 @@
+﻿#include "stdafx.h"
+#include "Snake.h"
+
+Snake::Snake(Rect Inital_) : move_dir(Vec2(1,0))
+{
+	body.push_back(Inital_);
+}
+bool Snake::CollisionTest(int startX, int startY, int width, int height)
+{
+	if(startX > body.front().x || body.front().x >= startX + width)
+	{
+		return false;
+	}
+	if (startY > body.front().y || startY + height <= body.front().y)
+	{
+		return false;
+	}
+
+	for (int i = 1; i < body.size(); i++) {
+		if (body.at(i) == body.front()) {
+			return false;
+		}
+	}
+	return true;
+}
+void Snake::body_add(Rect a)
+{
+	body.push_back(a);
+}
+void Snake::SetUp()
+{
+	move_dir = Vec2(0, 1);
+}
+
+void Snake::move()
+{
+	Rect front = body.front();
+	if (move_dir == Vec2(1, 0)) {
+		body.push_front(Rect{front.x + 51 , front.y , front.w , front.h});
+	}
+	if (move_dir == Vec2(-1, 0)) {
+		body.push_front(Rect{ front.x - 51 , front.y , front.w , front.h });
+	}
+	if (move_dir == Vec2(0, -1)) {
+		body.push_front(Rect{ front.x , front.y + 51 , front.w , front.h });
+	}
+	if (move_dir == Vec2(0, 1)) {
+		body.push_front(Rect{ front.x , front.y - 51 , front.w , front.h });
+	}
+	body.pop_back();
+}
+
+void Snake::SetDown()
+{
+	move_dir = Vec2(0, -1);
+}
+
+void Snake::SetRight()
+{
+	move_dir = Vec2(1, 0);
+}
+
+void Snake::draw()
+{
+	int h = 0;
+	for (auto elem : body) {
+		h += 10;
+		elem.draw(ColorF{ HSV(h) });
+	}
+}
+
+void Snake::SetLeft()
+{
+	move_dir = Vec2(-1, 0);
+}
---
+++ Siv_example/Snake.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+class Snake
+{
+public:
+	Array<Rect> body;
+	int length = 1;
+private:
+	Vec2 move_dir;
+
+public:
+
+	Snake(Rect Inital_Pos);
+	bool CollisionTest(int startX, int startY, int width, int height);
+	void body_add(Rect a);
+	void draw();
+	void move();
+	void SetUp();
+	void SetDown();
+	void SetRight();
+	void SetLeft();
+};
